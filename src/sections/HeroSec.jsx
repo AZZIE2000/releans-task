79d610fb3e7c98d76d1624f681dca594944d4d80
AZZIE2000 <azzam.faraj0@gmail.com>
2022-12-21 22:09:50 +0300

    fixed a swiper

--- src/sections/HeroSec.jsx
+++ src/sections/HeroSec.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -7,10 +7,22 @@ import { Autoplay } from "swiper";
 import { HiOutlineArrowLeft, HiOutlineArrowRight } from 'react-icons/hi';
 export default function HeroSec() {
     const [slide, setSlide] = useState(0)
+    const swiperRef = useRef()
+    const handleNextClick = () => {
+        if (swiperRef.current && swiperRef.current.swiper) {
+            swiperRef.current.swiper.slideNext();
+        }
+    };
+    const handlePrevClick = () => {
+        if (swiperRef.current && swiperRef.current.swiper) {
+            swiperRef.current.swiper.slidePrev();
+        }
+    };
     return (
         <div className='  relative'>
 
             <Swiper
+                ref={swiperRef}
                 autoplay={{
                     delay: 3000,
                     disableOnInteraction: false,
@@ -40,9 +52,9 @@ export default function HeroSec() {
             </Swiper>
             <div className='absolute  flex gap-5 items-center backdrop-blur  rounded  right-1/2 translate-x-1/2 bottom-10 z-40 '>
 
-                <HiOutlineArrowLeft className='md:h-16 md:w-16  h-10 w-10 text-white hover:text-red-600 cursor-pointer transform transition-all duration-700 hover:-translate-x-2' />
+                <HiOutlineArrowLeft onClick={handleNextClick} className='md:h-16 md:w-16  h-10 w-10 text-white hover:text-red-600 cursor-pointer transform transition-all duration-700 hover:-translate-x-2' />
                 <span className='text-white font-bold text-lg'>{slide + 1}/3</span>
-                <HiOutlineArrowRight className='md:h-16 md:w-16 h-10 w-10 text-white hover:text-red-600 cursor-pointer transform transition-all duration-700 hover:translate-x-2 ' />
+                <HiOutlineArrowRight onClick={handlePrevClick} className='md:h-16 md:w-16 h-10 w-10 text-white hover:text-red-600 cursor-pointer transform transition-all duration-700 hover:translate-x-2 ' />
             </div>
         </div>
     )
